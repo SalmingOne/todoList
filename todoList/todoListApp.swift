b9e852bfd4a68b556cd3ee8474d913841c268036
Wadim Smirnov <corp.wnsmir@gmail.com>
2023-08-22 15:36:49 +0300

    Initial Commit

---
+++ todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by Вадим on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
